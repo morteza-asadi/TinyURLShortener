06cef2cf5c7deed081ff74d1d9abca93c9bdc4ed
Morteza Asadi <morteza.asadi@live.com>
2024-09-10 16:06:01 +0200

    feat: Add Swagger functionality for API documentation

--- src/TinyURLShortener.API/Program.cs
+++ src/TinyURLShortener.API/Program.cs
@@ -3,9 +3,15 @@ using TinyURLShortener.API.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("TinyUrlShortener"));
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
